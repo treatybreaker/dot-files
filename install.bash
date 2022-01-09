f12472c28ceb80bea3b8473fb51eb3ec2510b2cd
Price Hiller <price@vircio.com>
2022-01-09 01:37:00 -0600

    Correct perms for rsync

--- install.bash
+++ install.bash
@@ -356,8 +356,7 @@ source_installer() {
         ./nvim.appimage --appimage-extract
         if sudo -v >/dev/null 2>&1; then
             sudo chown -R "root:root" "squashfs-root"
-            sudo rsync -a "./squashfs-root/usr" "/usr"
-            sudo rm -rf "squashfs-root"
+            sudo rsync -a "./squashfs-root/usr/" "/usr/"
         else
             log "warning" "Unable to add neovim to path from $(important "${squashfs-root}"), did not have sudo permissions"
         fi
