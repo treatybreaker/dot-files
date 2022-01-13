e4ea7dd4fb5f992c4caaac2a8652ed28d75f9914
Price Hiller <price@vircio.com>
2022-01-12 21:58:37 -0600

    Better handling of plugins preloading

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -1,3 +1,10 @@
+local preload_good, preload = pcall(require, "plugins.preload")
+if not preload_good then
+  print("Preload unable to load...")
+end
+	
+require("core.theme").setup()
 require("core.options").setup()
 require("core.mappings").setup()
-require("core.theme").setup()
+
+
