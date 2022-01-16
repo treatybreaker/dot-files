f6beebeee471738d40a55dc84f78f6ff81f34ce1
Price Hiller <price@vircio.com>
2022-01-16 01:22:42 -0600

    [ p10k ] - include wifi speed

--- .p10k.zsh
+++ .p10k.zsh
@@ -99,6 +99,7 @@
     # swap                  # used swap
     # todo                  # todo items (https://github.com/todotxt/todo.txt-cli)
     public_ip               # public IP address
+    wifi                    # wifi speed
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     time                    # current time
@@ -107,7 +108,6 @@
     # ip                    # ip address and bandwidth usage for a specified network interface
     # proxy                 # system-wide http/https/ftp proxy
     # battery               # internal battery
-    # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
 
