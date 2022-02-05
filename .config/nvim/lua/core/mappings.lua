a9d540eb8e30bbd83582dba1c91705ca7220302b
Price Hiller <price@vircio.com>
2022-02-05 02:56:46 -0600

    [ Mappings ] Better split management

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -12,6 +12,19 @@ M.setup = function()
     -- Spell Checking
     map("n", "<leader>st",":set spell!<CR>")
 
+    -- Better split movement
+    map("n", "<C-l>", "<C-w>l")
+    map("n", "<C-h>", "<C-w>h")
+    map("n", "<C-k>", "<C-w>k")
+    map("n", "<C-j>", "<C-w>j")
+
+    -- Better Split Opening
+    map("n", "<C-s>", "<C-w>s")
+    map("n", "<C-v>", "<C-w>v")
+
+    -- Better split closing
+    map("n", "<C-x>", "<C-w>c")
+
 end
 
 return M
